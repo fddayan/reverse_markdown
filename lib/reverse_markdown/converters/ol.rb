044cff4c4a86454e3455a399f9d7ed87c5462907
Federico Dayan <federico.dayan@gmail.com>
2017-10-20 14:15:53 -0300

    adding extra blank line at the end of OL and UL

--- lib/reverse_markdown/converters/ol.rb
+++ lib/reverse_markdown/converters/ol.rb
@@ -3,7 +3,8 @@ module ReverseMarkdown
     class Ol < Base
       def convert(node, state = {})
         ol_count = state.fetch(:ol_count, 0) + 1
-        "\n" << treat_children(node, state.merge(ol_count: ol_count))
+        children_content = treat_children(node, state.merge(ol_count: ol_count))
+        "\n\n#{children_content}\n\n"  
       end
     end
 
